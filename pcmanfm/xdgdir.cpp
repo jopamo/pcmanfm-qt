a64768b863f1ac7e16fbf4456372fcc7e15d600e
Paul Moses <p@1g4.org>
2025-11-29 20:21:40 -0600

    Reformat XDG directory utilities implementation
    Clean up code formatting in xdgdir.cpp by standardizing spacing around
    parentheses and braces. Reorder include directives alphabetically for
    better organization. Update copyright header to include file description.

--- pcmanfm/xdgdir.cpp
+++ pcmanfm/xdgdir.cpp
@@ -1,33 +1,21 @@
 /*
- *    Copyright (C) 2013  Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
- *
- *    This program is free software; you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation; either version 2 of the License, or
- *    (at your option) any later version.
- *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License along
- *    with this program; if not, write to the Free Software Foundation, Inc.,
- *    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * XDG directory utilities implementation
+ * pcmanfm/xdgdir.cpp
  */
 
 #include "xdgdir.h"
-#include <QStandardPaths>
-#include <QRegularExpression>
-#include <QFile>
+
 #include <QDir>
+#include <QFile>
+#include <QRegularExpression>
 #include <QSaveFile>
+#include <QStandardPaths>
 
 static const QRegularExpression desktopRegex(QStringLiteral("XDG_DESKTOP_DIR=\"([^\n]*)\""));
 
 QString XdgDir::readUserDirsFile() {
     QFile file(QStandardPaths::writableLocation(QStandardPaths::ConfigLocation) + QStringLiteral("/user-dirs.dirs"));
-    if(file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
         QByteArray data = file.readAll();
         file.close();
         return QString::fromLocal8Bit(data);
@@ -37,11 +25,11 @@ QString XdgDir::readUserDirsFile() {
 
 QString XdgDir::readDesktopDir() {
     QString str = readUserDirsFile();
-    if(!str.isEmpty()) {
+    if (!str.isEmpty()) {
         QRegularExpressionMatch match;
-        if(str.lastIndexOf(desktopRegex, -1, &match) != -1) {
+        if (str.lastIndexOf(desktopRegex, -1, &match) != -1) {
             str = match.captured(1);
-            if(str.startsWith(QStringLiteral("$HOME"))) {
+            if (str.startsWith(QStringLiteral("$HOME"))) {
                 str = QStandardPaths::writableLocation(QStandardPaths::HomeLocation) + str.mid(5);
             }
             return str;
@@ -52,27 +40,25 @@ QString XdgDir::readDesktopDir() {
 
 void XdgDir::setDesktopDir(QString path) {
     QString home = QStandardPaths::writableLocation(QStandardPaths::HomeLocation);
-    if(path.startsWith(home)) {
+    if (path.startsWith(home)) {
         path = QStringLiteral("$HOME") + path.mid(home.length());
     }
     QString str = readUserDirsFile();
     QString line = QStringLiteral("XDG_DESKTOP_DIR=\"") + path + QLatin1Char('\"');
-    if(str.contains(desktopRegex)) {
+    if (str.contains(desktopRegex)) {
         str.replace(desktopRegex, line);
-    }
-    else {
-        if(!str.endsWith(QLatin1Char('\n'))) {
+    } else {
+        if (!str.endsWith(QLatin1Char('\n'))) {
             str += QLatin1Char('\n');
         }
         str += line + QLatin1Char('\n');
     }
     QString dir = QStandardPaths::writableLocation(QStandardPaths::ConfigLocation);
-    if(QDir().mkpath(dir)) { // write the file
+    if (QDir().mkpath(dir)) {  // write the file
         QSaveFile file(dir + QStringLiteral("/user-dirs.dirs"));
-        if(file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
             file.write(str.toLocal8Bit());
             file.commit();
         }
     }
 }
-
