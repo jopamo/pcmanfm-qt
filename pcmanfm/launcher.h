02737cfe137fcfe2d90097891f35f190993b2485
Paul Moses <p@1g4.org>
2025-11-29 20:19:45 -0600

    Refactor launcher header formatting and access specifiers
    Update file header comment to be more descriptive and consistent
    Standardize indentation for public, protected, and private access specifiers
    Condense simple getter and setter method implementations to single lines
    Add namespace comment for PCManFM and use consistent include guard formatting

--- pcmanfm/launcher.h
+++ pcmanfm/launcher.h
@@ -1,21 +1,6 @@
 /*
- * <one line to give the library's name and an idea of what it does.>
- * Copyright (C) 2014  <copyright holder> <email>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- *
+ * File launcher header
+ * pcmanfm/launcher.h
  */
 
 #ifndef PCMANFM_LAUNCHER_H
@@ -28,36 +13,28 @@ namespace PCManFM {
 class MainWindow;
 
 class Launcher : public Fm::FileLauncher {
-public:
+   public:
     Launcher(MainWindow* mainWindow = nullptr);
     ~Launcher();
 
-    bool hasMainWindow() const {
-        return mainWindow_ != nullptr;
-    }
+    bool hasMainWindow() const { return mainWindow_ != nullptr; }
 
-    void openInNewTab() {
-        openInNewTab_ = true;
-    }
+    void openInNewTab() { openInNewTab_ = true; }
 
-    bool openWithDefaultFileManager() const {
-        return openWithDefaultFileManager_;
-    }
-    void setOpenWithDefaultFileManager(bool open) {
-        openWithDefaultFileManager_ = open;
-    }
+    bool openWithDefaultFileManager() const { return openWithDefaultFileManager_; }
+    void setOpenWithDefaultFileManager(bool open) { openWithDefaultFileManager_ = open; }
 
-protected:
+   protected:
     bool openFolder(GAppLaunchContext* ctx, const Fm::FileInfoList& folderInfos, Fm::GErrorPtr& err) override;
     void launchedFiles(const Fm::FileInfoList& files) const override;
     void launchedPaths(const Fm::FilePathList& paths) const override;
 
-private:
+   private:
     MainWindow* mainWindow_;
     bool openInNewTab_;
     bool openWithDefaultFileManager_;
 };
 
-}
+}  // namespace PCManFM
 
-#endif // PCMANFM_LAUNCHER_H
+#endif  // PCMANFM_LAUNCHER_H
