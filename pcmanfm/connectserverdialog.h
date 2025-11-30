36d93a769677e3cde11bfff3c0744cf5464dde1b
Paul Moses <p@1g4.org>
2025-11-29 20:19:37 -0600

    Reformat ConnectServerDialog header with consistent indentation
    Standardize code style by converting tabs to spaces and adjusting indentation
    levels throughout the header file. Maintain all functionality while improving
    readability and consistency with project coding standards.

--- pcmanfm/connectserverdialog.h
+++ pcmanfm/connectserverdialog.h
@@ -1,38 +1,44 @@
+/*
+ * Connect to server dialog header
+ * pcmanfm/connectserverdialog.h
+ */
+
 #ifndef CONNECTSERVERDIALOG_H
 #define CONNECTSERVERDIALOG_H
 
 #include <QDialog>
 #include <QList>
+
 #include "ui_connect.h"
 
 namespace PCManFM {
 
 class ConnectServerDialog : public QDialog {
-  Q_OBJECT
+    Q_OBJECT
 
-public:
-  ConnectServerDialog(QWidget* parent=nullptr);
-  virtual ~ConnectServerDialog();
+   public:
+    ConnectServerDialog(QWidget* parent = nullptr);
+    virtual ~ConnectServerDialog();
 
-  QString uriText();
+    QString uriText();
 
-private:
-  struct ServerType {
-    QString name;
-    const char* scheme;
-    int defaultPort;
-    bool canAnonymous;
-  };
+   private:
+    struct ServerType {
+        QString name;
+        const char* scheme;
+        int defaultPort;
+        bool canAnonymous;
+    };
 
-private Q_SLOTS:
-  void onCurrentIndexChanged(int index);
-  void checkInput();
+   private Q_SLOTS:
+    void onCurrentIndexChanged(int index);
+    void checkInput();
 
-private:
-  Ui::ConnectServerDialog ui;
-  QList<ServerType> serverTypes;
+   private:
+    Ui::ConnectServerDialog ui;
+    QList<ServerType> serverTypes;
 };
 
-} // namespace PCManFM
+}  // namespace PCManFM
 
-#endif // CONNECTSERVERDIALOG_H
+#endif  // CONNECTSERVERDIALOG_H
