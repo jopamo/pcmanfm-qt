516a803cc2ddc4dd2cfb4b0be7243d2478808ef4
Paul <p@1g4.org>
2025-11-30 21:11:41 -0600

    Refactor: Remove legacy libfm fallback for new folder creation

--- pcmanfm/mainwindow.cpp
+++ pcmanfm/mainwindow.cpp
@@ -195,44 +195,11 @@ void MainWindow::on_actionNewFolder_triggered() {
             if (!dir.mkdir(name)) {
                 QMessageBox::warning(this, tr("Error"), tr("Failed to create folder \"%1\"").arg(name));
             }
-
-            return;
         }
+        else {
+            QMessageBox::warning(this, tr("Operation Not Supported"),
 
-        // Legacy fallback for non-native paths
-
-        // Get a Folder object for the current directory
-
-        // Fm::Folder::fromPath returns a std::shared_ptr<Fm::Folder>
-
-        auto folder = Fm::Folder::fromPath(page->path());
-
-        if (!folder) {
-            QMessageBox::warning(this, tr("Error"), tr("Failed to access current folder"));
-
-            return;
-        }
-
-        GError* err = nullptr;
-
-        QByteArray utf8Name = name.toUtf8();
-
-        // makeDirectory signature in libfm-qt6 core:
-
-        // bool makeDirectory(const char* name, GError** error, GCancellable* cancellable = nullptr)
-
-        if (!folder->makeDirectory(utf8Name.constData(), &err)) {
-            QString msg = tr("Failed to create folder \"%1\"").arg(name);
-
-            if (err) {
-                msg += QLatin1Char('\n');
-
-                msg += QString::fromUtf8(err->message);
-
-                g_error_free(err);
-            }
-
-            QMessageBox::warning(this, tr("Error"), msg);
+                                 tr("Creating folders is currently supported only on local file systems."));
         }
     }
 }
